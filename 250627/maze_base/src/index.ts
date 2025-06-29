faa4dd22809fce9f01db2b0b6a7ef07b2bb7b270
suchan <uyhj0804@ajou.ac.kr>
2025-06-29 23:24:59 +0900

    maze_base

--- 250627/maze_base/src/index.ts
+++ 250627/maze_base/src/index.ts
@@ -1,116 +1,74 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 function execute() {
-	const data = fs.readFileSync(path.join(__dirname, '../map/maze_10x10.txt'));
-	const map = data.toString();
-	const startTime = performance.now();
-	const result = findPath(map);
-	const duration = performance.now() - startTime;
-	console.log(`Duration: ${duration} ms`);
-	console.log(`Result: ${result}`);
+  const data = fs.readFileSync(path.join(__dirname, "../map/maze_10x10.txt"));
+  const map = data.toString();
+  const startTime = performance.now();
+  const result = findPath(map);
+  const duration = performance.now() - startTime;
+  console.log(`Duration: ${duration} ms`);
+  console.log(`Result: ${result}`);
 }
 
-function bfs(map: any, start: any, port?: string) {
-	const rows = map[0].length;
-	let visited = new Array(rows); // 행 생성
-	for (let i = 0; i < rows; i++) {
-		visited[i] = new Array(rows); // 열 생성
-	}
-	const queue = [[start[0], start[1], 0]];
-	visited[start[0]][start[1]] = true;
-	const move = [
-		[0, 1],
-		[0, -1],
-		[1, 0],
-		[-1, 0],
-	];
-
-	while (queue.length > 0) {
-		const item = queue.shift();
-		if (!item) {
-			console.log('Empty Queue');
-			continue;
-		}
-		const [row, col, distance] = item;
-
-		for (const [x, y] of move) {
-			const newX: number = row + y;
-			const newY: number = col + x;
-			const reg = /^[0-9]$/;
-			if (map[row][col] === 'E') {
-				console.log('find', row, col, port);
-				return { distance, row, col, port: map[row][col] };
-			}
-			if (reg.test(map[row][col])) {
-				console.log('map[row][col]');
-				return { distance, row, col, port: map[row][col] };
-			}
-			if (
-				newX >= 0 &&
-				newX < rows &&
-				newY >= 0 &&
-				newY < rows &&
-				!visited[newX][newY] &&
-				!(map[newX][newY] === '#')
-			) {
-				visited[newX][newY] = true;
-				queue.push([newX, newY, distance + 1]);
-			}
-		}
-	}
-	return { distance: -1, row: -1, col: -1, port: 'none' };
+function findPath(map: any) {
+  let result: string = "";
+  const maps = (map.split("\n") as string[])
+    .filter((v) => !!v)
+    .map((v) => v.split(" ").filter((v) => !!v));
+  const N = maps[0].length;
+  const M = maps.length;
+  const visited = new Array(M).fill(0).map(() => new Array(N).fill(false));
+  const portalMap: { [key in string]: { x: number; y: number }[] } = {};
+  for (let i = 0; i < M; i++) {
+    for (let j = 0; j < N; j++) {
+      if (!isNaN(Number(maps[j][i]))) {
+        if (portalMap[maps[j][i]]) {
+          portalMap[maps[j][i]].push({ x: i, y: j });
+        } else {
+          portalMap[maps[j][i]] = [{ x: i, y: j }];
+        }
+      }
+    }
+  }
+  const queue: { x: number; y: number; count: number }[] = [];
+  queue.push({ x: 0, y: 0, count: 0 });
+  visited[0][0] = true;
+  let distance = -1;
+  while (queue.length > 0) {
+    const { x, y, count } = queue.shift()!;
+    if (maps[y][x] === ".E") {
+      console.log("exit");
+      distance = count;
+      break;
+    }
+    const dx = [1, 0, -1, 0];
+    const dy = [0, 1, 0, -1];
+    for (let i = 0; i < 4; i++) {
+      const newX = x + dx[i];
+      const newY = y + dy[i];
+      if (newX >= 0 && newX < N && newY >= 0 && newY < M) {
+        if (visited[newY][newX] === false && maps[newY][newX] !== "#") {
+          visited[newY][newX] = true;
+          if (!isNaN(Number(maps[newY][newX]))) {
+            const portals = portalMap[maps[newY][newX]];
+            const portal = portals.find((d) => d.x !== newX && d.y !== newY)!;
+            if (!visited[portal.y][portal.x]) {
+              queue.push({ x: portal.x, y: portal.y, count: count + 1 });
+              visited[portal.y][portal.x] = true;
+            }
+          } else {
+            queue.push({
+              x: newX,
+              y: newY,
+              count: count + 1,
+            });
+          }
+        }
+      }
+    }
+  }
+  return distance === -1 ? "NO" : String(distance);
 }
 
-function findPath(map: any): string {
-	let result: string = '';
-	let cnt = 0;
-	const maps = map.replaceAll(' ', '').replace('.S', 'S').replace('.E', 'E');
-	const line = Number(maps.split('').findIndex((x: string) => x == '\n'));
-	const arrayMaps = maps.replaceAll('\n', '').split('');
-	let arr = new Array(line); // 행 생성
-	for (let i = 0; i < line; i++) {
-		arr[i] = new Array(line); // 열 생성
-	}
-	for (let i = 0; i < line; i++) {
-		for (let j = 0; j < line; j++) {
-			arr[i][j] = arrayMaps[cnt];
-			cnt++;
-		}
-	}
-	let start = [0, 0];
-	let ports = undefined;
-	let subMatrix = [];
-	let { distance, row, col, port } = bfs(arr, start);
-	let portValue = distance;
-
-	while (true) {
-		if (ports == 'E') {
-			result = distance.toString();
-			break;
-		} else {
-			subMatrix = getSubMatrix(arr, row, line, col, line);
-			// subMatrix[row][col] = 'S';
-			const obj = bfs(subMatrix, start);
-			portValue += obj.distance;
-			ports = obj.port;
-		}
-	}
-	// console.log(result);
-	return result;
-}
 execute();
-
-function getSubMatrix(
-	matrix: any,
-	rowStart: number,
-	rowEnd: number,
-	colStart: number,
-	colEnd: number
-) {
-	const subMatrix: any = [];
-	for (let i = rowStart; i < rowEnd; i++) {
-		subMatrix.push(matrix[i].slice(colStart, colEnd));
-	}
-	return subMatrix;
-}
